ce6134a5996c6a3f3e0ab15e0e8baaebc21f2bf6
zjun <565061763@qq.com>
2017-09-19 19:23:23 +0800

    重构

--- libCore/src/main/java/com/fanwe/library/utils/SDToast.java
+++ libCore/src/main/java/com/fanwe/library/utils/SDToast.java
@@ -9,49 +9,51 @@ import com.fanwe.library.SDLibrary;
 
 /**
  * toast显示类，可以在子线程直接调用
- * 
  */
 public class SDToast
 {
-	private static Toast sToast;
+    public static final Handler S_HANDLER = new Handler(Looper.getMainLooper());
 
-	public static Handler sHandler = new Handler(Looper.getMainLooper());
+    private static Toast sToast;
 
-	public static void showToast(String text)
-	{
-		showToast(text, Toast.LENGTH_LONG);
-	}
+    public static void showToast(CharSequence text)
+    {
+        showToast(text, Toast.LENGTH_LONG);
+    }
 
-	public static void showToast(final String text, final int duration)
-	{
-		if (Looper.myLooper() == Looper.getMainLooper())
-		{
-			show(text, duration);
-		} else
-		{
-			sHandler.post(new Runnable()
-			{
-				@Override
-				public void run()
-				{
-					show(text, duration);
-				}
-			});
-		}
-	}
+    public static void showToast(final CharSequence text, final int duration)
+    {
+        if (Looper.myLooper() == Looper.getMainLooper())
+        {
+            showInternal(text, duration);
+        } else
+        {
+            S_HANDLER.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    showInternal(text, duration);
+                }
+            });
+        }
+    }
 
-	private static void show(String text, int duration)
-	{
-		if (TextUtils.isEmpty(text))
-		{
-			return;
-		}
-		if (sToast != null)
-		{
-			sToast.cancel();
-		}
-		sToast = Toast.makeText(SDLibrary.getInstance().getContext(), text, duration);
-		sToast.show();
-	}
+    private static void showInternal(CharSequence text, int duration)
+    {
+        if (TextUtils.isEmpty(text))
+        {
+            return;
+        }
+        if (sToast != null)
+        {
+            sToast.setText(text);
+            sToast.setDuration(duration);
+        } else
+        {
+            sToast = Toast.makeText(SDLibrary.getInstance().getContext(), text, duration);
+        }
+        sToast.show();
+    }
 
 }
