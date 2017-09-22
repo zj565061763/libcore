f33cf8852fe351635b089306358ddf7d17fb75df
zjun <565061763@qq.com>
2017-09-22 13:37:13 +0800

    重构

--- libCore/src/main/java/com/fanwe/library/common/SDCookieManager.java
+++ libCore/src/main/java/com/fanwe/library/common/SDCookieManager.java
@@ -6,6 +6,7 @@ import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 
 import java.net.HttpCookie;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,6 +15,9 @@ import java.util.List;
  */
 public class SDCookieManager
 {
+    private static final String KEY_GROUP = ";";
+    private static final String KEY_PAIR = "=";
+
     private static SDCookieManager instance;
 
     public static SDCookieManager getInstance()
@@ -33,6 +37,38 @@ public class SDCookieManager
     public String getCookie(String url)
     {
         return CookieManager.getInstance().getCookie(url);
+    }
+
+    public List<HttpCookie> getHttpCookie(String url)
+    {
+        String cookie = getCookie(url);
+        if (TextUtils.isEmpty(cookie))
+        {
+            return null;
+        }
+        if (!cookie.contains(KEY_PAIR))
+        {
+            return null;
+        }
+        String[] arrCookie = cookie.split(KEY_GROUP);
+        if (arrCookie == null || arrCookie.length <= 0)
+        {
+            return null;
+        }
+        List<HttpCookie> listCookie = new ArrayList<>();
+        for (String item : arrCookie)
+        {
+            if (item.contains(KEY_PAIR))
+            {
+                String[] arrPair = item.split(KEY_PAIR);
+                if (arrPair != null && arrPair.length == 2)
+                {
+                    HttpCookie httpCookie = new HttpCookie(arrPair[0], arrPair[1]);
+                    listCookie.add(httpCookie);
+                }
+            }
+        }
+        return listCookie;
     }
 
     public void setCookie(String url, String value)
