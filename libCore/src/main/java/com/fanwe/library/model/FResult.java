51c0d16801d636fdae760259c39ce504a0b44cdb
zjun <565061763@qq.com>
2018-03-06 15:29:51 +0800

    新增FResult

---
+++ libCore/src/main/java/com/fanwe/library/model/FResult.java
@@ -0,0 +1,95 @@
+package com.fanwe.library.model;
+
+/**
+ * Created by zhengjun on 2018/3/6.
+ */
+public class FResult<T>
+{
+    private final boolean mIsSuccessful;
+
+    private T mData;
+
+    private int mCode;
+    private String mDescription;
+
+    public FResult(boolean isSuccessful)
+    {
+        mIsSuccessful = isSuccessful;
+    }
+
+    /**
+     * 设置结果数据
+     *
+     * @param data
+     * @return
+     */
+    public FResult<T> setData(T data)
+    {
+        mData = data;
+        return this;
+    }
+
+    /**
+     * 设置结果码
+     *
+     * @param code
+     * @return
+     */
+    public FResult<T> setCode(int code)
+    {
+        mCode = code;
+        return this;
+    }
+
+    /**
+     * 设置结果描述
+     *
+     * @param description
+     * @return
+     */
+    public FResult<T> setDescription(String description)
+    {
+        mDescription = description;
+        return this;
+    }
+
+    /**
+     * 返回当前结果对象是否是成功的结果
+     *
+     * @return
+     */
+    public boolean isSuccessful()
+    {
+        return mIsSuccessful;
+    }
+
+    /**
+     * 返回结果数据
+     *
+     * @return
+     */
+    public T getData()
+    {
+        return mData;
+    }
+
+    /**
+     * 返回结果码
+     *
+     * @return
+     */
+    public int getCode()
+    {
+        return mCode;
+    }
+
+    /**
+     * 返回结果描述
+     *
+     * @return
+     */
+    public String getDescription()
+    {
+        return mDescription;
+    }
+}
