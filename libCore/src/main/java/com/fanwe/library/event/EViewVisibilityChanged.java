fd9ecd79f2b23a17d1d9b5ddd7c254d0c3ca20d7
zjun <565061763@qq.com>
2018-03-07 10:58:40 +0800

    新增EViewVisibilityChanged

---
+++ libCore/src/main/java/com/fanwe/library/event/EViewVisibilityChanged.java
@@ -0,0 +1,12 @@
+package com.fanwe.library.event;
+
+import android.view.View;
+
+/**
+ * View的可见状态发生变化
+ */
+public class EViewVisibilityChanged
+{
+    public View view;
+    public int visibility;
+}
