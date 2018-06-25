13f9a1030c64435b2b916264a9fa32ca7ed8950b
zjun <565061763@qq.com>
2018-06-25 14:28:37 +0800

    1.1.19

--- libCore/src/main/java/com/fanwe/library/view/SDDefaultStringTextView.java
+++ libCore/src/main/java/com/fanwe/library/view/SDDefaultStringTextView.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-/**
- * 用{@link FPlaceholderTextView}替代
- */
 @Deprecated
 public abstract class SDDefaultStringTextView extends TextView
 {
