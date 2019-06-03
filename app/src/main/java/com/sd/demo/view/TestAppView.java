3e9370778dd58582ae2fee22ca8315c6dae73816
zjun <565061763@qq.com>
2019-06-03 17:57:35 +0800

    demo

---
+++ app/src/main/java/com/sd/demo/view/TestAppView.java
@@ -0,0 +1,39 @@
+package com.sd.demo.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.sd.libcore.stream.activity.ActivityDestroyedStream;
+import com.sd.libcore.stream.activity.ActivityResumedStream;
+import com.sd.libcore.stream.activity.ActivityStoppedStream;
+import com.sd.libcore.view.FAppView;
+
+public class TestAppView extends FAppView implements ActivityResumedStream, ActivityStoppedStream, ActivityDestroyedStream
+{
+    public static final String TAG = TestAppView.class.getSimpleName();
+
+    public TestAppView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity)
+    {
+        Log.i(TAG, "onActivityResumed:" + activity);
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity)
+    {
+        Log.i(TAG, "onActivityStopped:" + activity);
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity)
+    {
+        Log.i(TAG, "onActivityDestroyed:" + activity);
+    }
+}
