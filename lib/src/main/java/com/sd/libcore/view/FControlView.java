8190963c74444013661e03614a3c427972ed2702
zjun <565061763@qq.com>
2019-07-07 16:11:20 +0800

    1.3.0-rc12

---
+++ lib/src/main/java/com/sd/libcore/view/FControlView.java
@@ -0,0 +1,75 @@
+package com.sd.libcore.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.sd.lib.stream.FStream;
+import com.sd.lib.stream.FStreamManager;
+import com.sd.libcore.activity.FActivity;
+import com.sd.libcore.stream.activity.ActivityDestroyedStream;
+
+/**
+ * 如果手动的new对象的话Context必须传入Activity对象
+ */
+public class FControlView extends FViewGroup implements ActivityDestroyedStream
+{
+    public FControlView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        FStreamManager.getInstance().bindView(this, this);
+    }
+
+    public FActivity getFActivity()
+    {
+        final Activity activity = getActivity();
+        return activity instanceof FActivity ? (FActivity) activity : null;
+    }
+
+    @Override
+    public Object getTagForStream(Class<? extends FStream> clazz)
+    {
+        return getStreamTag();
+    }
+
+    public final String getStreamTag()
+    {
+        final FActivity fActivity = getFActivity();
+        if (fActivity != null)
+            return fActivity.getStreamTag();
+
+        final Activity activity = getActivity();
+        if (activity != null)
+            return activity.toString();
+
+        return getStreamTagView();
+    }
+
+    public final String getStreamTagView()
+    {
+        final String className = getClass().getName();
+        final String hashCode = Integer.toHexString(System.identityHashCode(this));
+        return className + "@" + hashCode;
+    }
+
+    public void showProgressDialog(String msg)
+    {
+        final FActivity fActivity = getFActivity();
+        if (fActivity != null)
+            fActivity.showProgressDialog(msg);
+    }
+
+    public void dismissProgressDialog()
+    {
+        final FActivity fActivity = getFActivity();
+        if (fActivity != null)
+            fActivity.dismissProgressDialog();
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity)
+    {
+
+    }
+}
---
+++ lib/src/main/java/com/sd/libcore/view/FViewGroup.java
@@ -0,0 +1,168 @@
+package com.sd.libcore.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.FrameLayout;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * 如果手动的new对象的话Context必须传入Activity对象
+ */
+public class FViewGroup extends FrameLayout implements View.OnClickListener
+{
+    /**
+     * 设置是否消费掉触摸事件，true-事件不会透过view继续往下传递
+     */
+    private boolean mConsumeTouchEvent = false;
+    private WeakReference<ViewGroup> mContainer;
+
+    public FViewGroup(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    /**
+     * 设置内容View布局
+     *
+     * @param layoutId 布局id
+     */
+    public void setContentView(int layoutId)
+    {
+        removeAllViews();
+        LayoutInflater.from(getContext()).inflate(layoutId, this, true);
+    }
+
+    /**
+     * 设置内容View
+     *
+     * @param contentView
+     */
+    public void setContentView(View contentView)
+    {
+        removeAllViews();
+        addView(contentView);
+    }
+
+    /**
+     * 设置是否消费掉触摸事件
+     *
+     * @param consumeTouchEvent true-消费掉事件，事件不会透过view继续往下传递
+     */
+    public void setConsumeTouchEvent(boolean consumeTouchEvent)
+    {
+        mConsumeTouchEvent = consumeTouchEvent;
+    }
+
+    /**
+     * 设置父容器
+     *
+     * @param container
+     * @return
+     */
+    public FViewGroup setContainer(View container)
+    {
+        if (container == null)
+        {
+            mContainer = null;
+        } else
+        {
+            if (container instanceof ViewGroup)
+                mContainer = new WeakReference<>((ViewGroup) container);
+            else
+                throw new IllegalArgumentException("container must be instance of ViewGroup");
+        }
+        return this;
+    }
+
+    /**
+     * 返回设置的父容器
+     *
+     * @return
+     */
+    public ViewGroup getContainer()
+    {
+        return mContainer == null ? null : mContainer.get();
+    }
+
+    public Activity getActivity()
+    {
+        final Context context = getContext();
+        return context instanceof Activity ? (Activity) context : null;
+    }
+
+    /**
+     * 把View添加到设置的容器{@link #setContainer(View)}
+     *
+     * @param replace true-父容器仅保留当前View对象在容器中
+     */
+    public final void attach(boolean replace)
+    {
+        final ViewGroup viewGroup = getContainer();
+        if (viewGroup == null)
+            return;
+
+        if (getParent() != viewGroup)
+        {
+            if (replace)
+                viewGroup.removeAllViews();
+
+            detach();
+            viewGroup.addView(this);
+        } else
+        {
+            if (replace)
+            {
+                final int count = viewGroup.getChildCount();
+                if (count != 1)
+                {
+                    for (int i = count - 1; i >= 0; i--)
+                    {
+                        final View item = viewGroup.getChildAt(i);
+                        if (item != this)
+                            viewGroup.removeView(item);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 把当前View从父容器上移除
+     */
+    public final void detach()
+    {
+        final ViewParent parent = getParent();
+        if (parent instanceof ViewGroup)
+        {
+            try
+            {
+                ((ViewGroup) parent).removeView(this);
+            } catch (Exception e)
+            {
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if (mConsumeTouchEvent)
+        {
+            super.onTouchEvent(event);
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
