0a7b6fda13ce880a87a6a3ed3ae76e470f40d7c1
zjun <565061763@qq.com>
2019-12-09 16:32:07 +0800

    restore business

---
+++ lib/src/main/java/com/sd/libcore/business/FBusiness.java
@@ -0,0 +1,45 @@
+package com.sd.libcore.business;
+
+import com.sd.lib.stream.FStream;
+import com.sd.libcore.business.stream.BSProgress;
+
+public abstract class FBusiness
+{
+    private final String mTag;
+
+    public FBusiness(String tag)
+    {
+        mTag = tag;
+    }
+
+    /**
+     * 返回当前业务类的标识
+     *
+     * @return
+     */
+    public final String getTag()
+    {
+        return mTag;
+    }
+
+    /**
+     * 创建一个流代理对象返回
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    protected final <T extends FStream> T getStream(Class<T> clazz)
+    {
+        return new FStream.ProxyBuilder().setTag(getTag()).build(clazz);
+    }
+
+    protected final BSProgress getProgress()
+    {
+        return getStream(BSProgress.class);
+    }
+
+    public void onDestroy()
+    {
+    }
+}
