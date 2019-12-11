0d8a259ba6bd7dac6c87b109c53eecf0de045175
zjun <565061763@qq.com>
2019-12-11 11:51:03 +0800

    FControlView 默认监听 resume pause stop destroy

--- lib/src/main/java/com/sd/libcore/view/FControlView.java
+++ lib/src/main/java/com/sd/libcore/view/FControlView.java
@@ -5,14 +5,24 @@ import android.content.Context;
 import android.util.AttributeSet;
 
 import com.sd.lib.eventact.callback.ActivityDestroyedCallback;
+import com.sd.lib.eventact.callback.ActivityPausedCallback;
+import com.sd.lib.eventact.callback.ActivityResumedCallback;
+import com.sd.lib.eventact.callback.ActivityStoppedCallback;
 import com.sd.lib.eventact.observer.ActivityDestroyedObserver;
+import com.sd.lib.eventact.observer.ActivityPausedObserver;
+import com.sd.lib.eventact.observer.ActivityResumedObserver;
+import com.sd.lib.eventact.observer.ActivityStoppedObserver;
 import com.sd.libcore.activity.FActivity;
 import com.sd.libcore.activity.FStreamActivity;
 
 /**
  * 如果手动的new对象的话Context必须传入Activity对象
  */
-public class FControlView extends FViewGroup implements ActivityDestroyedCallback
+public class FControlView extends FViewGroup implements
+        ActivityResumedCallback,
+        ActivityPausedCallback,
+        ActivityStoppedCallback,
+        ActivityDestroyedCallback
 {
     public FControlView(Context context, AttributeSet attrs)
     {
@@ -52,6 +62,55 @@ public class FControlView extends FViewGroup implements ActivityDestroyedCallbac
             ((FActivity) activity).dismissProgressDialog();
     }
 
+    @Override
+    protected void onAttachedToWindow()
+    {
+        super.onAttachedToWindow();
+
+        final Activity activity = getActivity();
+        mActivityResumedObserver.register(activity);
+        mActivityPausedObserver.register(activity);
+        mActivityStoppedObserver.register(activity);
+        mActivityDestroyedObserver.register(activity);
+    }
+
+    @Override
+    protected void onDetachedFromWindow()
+    {
+        super.onDetachedFromWindow();
+        mActivityResumedObserver.unregister();
+        mActivityPausedObserver.unregister();
+        mActivityStoppedObserver.unregister();
+        mActivityDestroyedObserver.unregister();
+    }
+
+    private final ActivityResumedObserver mActivityResumedObserver = new ActivityResumedObserver()
+    {
+        @Override
+        public void onActivityResumed(Activity activity)
+        {
+            FControlView.this.onActivityResumed(activity);
+        }
+    };
+
+    private final ActivityPausedObserver mActivityPausedObserver = new ActivityPausedObserver()
+    {
+        @Override
+        public void onActivityPaused(Activity activity)
+        {
+            FControlView.this.onActivityPaused(activity);
+        }
+    };
+
+    private final ActivityStoppedObserver mActivityStoppedObserver = new ActivityStoppedObserver()
+    {
+        @Override
+        public void onActivityStopped(Activity activity)
+        {
+            FControlView.this.onActivityStopped(activity);
+        }
+    };
+
     private final ActivityDestroyedObserver mActivityDestroyedObserver = new ActivityDestroyedObserver()
     {
         @Override
@@ -62,22 +121,22 @@ public class FControlView extends FViewGroup implements ActivityDestroyedCallbac
     };
 
     @Override
-    protected void onAttachedToWindow()
+    public void onActivityResumed(Activity activity)
     {
-        super.onAttachedToWindow();
-        mActivityDestroyedObserver.register(getActivity());
     }
 
     @Override
-    protected void onDetachedFromWindow()
+    public void onActivityPaused(Activity activity)
     {
-        super.onDetachedFromWindow();
-        mActivityDestroyedObserver.unregister();
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity)
+    {
     }
 
     @Override
     public void onActivityDestroyed(Activity activity)
     {
-
     }
 }
