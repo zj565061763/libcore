3d1a62e02438b188f3bcb5c355a68f8b6b1cbd32
zjun <565061763@qq.com>
2020-09-04 16:19:59 +0800

    FStreamActivity

--- lib/src/main/java/com/sd/libcore/activity/FStreamActivity.java
+++ lib/src/main/java/com/sd/libcore/activity/FStreamActivity.java
@@ -1,10 +1,34 @@
 package com.sd.libcore.activity;
 
+import android.os.Bundle;
+
+import com.sd.lib.stream.FStream;
+import com.sd.lib.stream.FStreamManager;
 import com.sd.libcore.business.holder.FActivityBusinessHolder;
 import com.sd.libcore.business.holder.FBusinessHolder;
 
-public abstract class FStreamActivity extends FActivity
+public abstract class FStreamActivity extends FActivity implements FStream
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        FStreamManager.getInstance().register(this);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        FStreamManager.getInstance().unregister(this);
+    }
+
+    @Override
+    public Object getTagForStream(Class<? extends FStream> clazz)
+    {
+        return getStreamTag();
+    }
+
     public final String getStreamTag()
     {
         return FStreamActivity.this.toString();
