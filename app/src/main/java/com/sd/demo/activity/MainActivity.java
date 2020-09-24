22338e3efe5b56af13d65100bf427aff9580a8b0
zjun <565061763@qq.com>
2020-09-24 15:32:20 +0800

    demo

--- app/src/main/java/com/sd/demo/activity/MainActivity.java
+++ app/src/main/java/com/sd/demo/activity/MainActivity.java
@@ -3,14 +3,18 @@ package com.sd.demo.activity;
 import android.os.Bundle;
 
 import com.sd.demo.R;
+import com.sd.demo.databinding.ActMainBinding;
 import com.sd.libcore.activity.FStreamActivity;
 
 public class MainActivity extends FStreamActivity
 {
+    private ActMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+        mBinding = ActMainBinding.bind(getContentView());
     }
 }
