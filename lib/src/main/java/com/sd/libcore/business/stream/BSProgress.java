fd816e8bd10359f0c785258d44ac33d6f547c527
zjun <565061763@qq.com>
2021-03-01 10:44:36 +0800

    annotation

--- lib/src/main/java/com/sd/libcore/business/stream/BSProgress.java
+++ lib/src/main/java/com/sd/libcore/business/stream/BSProgress.java
@@ -1,10 +1,12 @@
 package com.sd.libcore.business.stream;
 
+import androidx.annotation.Nullable;
+
 import com.sd.lib.stream.FStream;
 
 public interface BSProgress extends FStream
 {
-    void bsShowProgress(String msg);
+    void bsShowProgress(@Nullable String msg);
 
     void bsHideProgress();
 }
--- lib/src/main/java/com/sd/libcore/business/stream/BSTipsCallback.java
+++ lib/src/main/java/com/sd/libcore/business/stream/BSTipsCallback.java
@@ -1,8 +1,10 @@
 package com.sd.libcore.business.stream;
 
+import androidx.annotation.Nullable;
+
 import com.sd.lib.stream.FStream;
 
 public interface BSTipsCallback extends FStream
 {
-    void bsShowTips(String tips);
+    void bsShowTips(@Nullable String tips);
 }
